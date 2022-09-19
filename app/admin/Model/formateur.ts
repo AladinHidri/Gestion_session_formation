0cf945b4507ca2915c61ecacacdd942b736dc321
aladin hidri <ahidri@spark-it.fr>
2022-09-19 18:21:43 +0100

    mon premier commit

---
+++ app/admin/Model/formateur.ts
@@ -0,0 +1,44 @@
+import {Formateur} from './formateur.model';
+
+export const FORMATEURITEMS: Formateur[] = [
+    {
+        idFormateur: 1,
+        nomFormateur: 'Zekri',
+        prenomFormateur: 'Sabrine',
+        adresseFormateur: 'Omrane supérieur',
+        emailFormateur: 'sabrinezekri@esprit.tn',
+        domaineFormateur: 'Informatque',
+
+
+    },
+    {
+        idFormateur: 2,
+        nomFormateur: 'Ghozia',
+        prenomFormateur: 'Abdesslam',
+        adresseFormateur: 'Omrane supérieur',
+        emailFormateur: 'ghoziaAbdesslam@esprit.tn',
+        domaineFormateur: 'Informatque',
+
+
+    },
+    {
+        idFormateur: 3,
+        nomFormateur: 'Zekri',
+        prenomFormateur: 'Sabrine',
+        adresseFormateur: 'Omrane supérieur',
+        emailFormateur: 'sabrinezekri@esprit.tn',
+        domaineFormateur: 'Informatque',
+
+
+    },
+    {
+        idFormateur: 4,
+        nomFormateur: 'Ghozia',
+        prenomFormateur: 'Abdesslam',
+        adresseFormateur: 'Omrane supérieur',
+        emailFormateur: 'ghoziaAbdesslam@esprit.tn',
+        domaineFormateur: 'Informatque',
+
+
+    }
+];
